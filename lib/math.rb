3e54ab6fe7f65c6534d8f84e82bde8a2b797b9c4
lively-bundle-0725 <tbandachit@gmail.com>
2020-03-01 00:44:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
+calculated = 0;
 def addition(num1, num2)
-
+  calculated = num1 + num2;
+  return calculated;
 end
 
 def subtraction(num1, num2)
-
+  calculated = num1 - num2;
+  return calculated;
 end
 
 def division(num1, num2)
-
+    calculated = num1 / num2;
+  return calculated;
 end
 
 def multiplication(num1, num2)
-
+  calculated = num1  * num2;
+  return calculated;
 end
 
 def modulo(num1, num2)
-
+  calculated = num1 % num2;
+  return calculated;
 end
-
+  
 def square_root(num)
-
+  calculated = num1 num2;
+  return calculated;
 end
